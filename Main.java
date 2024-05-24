6985575bde09f795ff84884063659b706bb65310
Rajat Pratap <chauhanrajat515@gmail.com>
2024-05-25 00:48:09 +0530

    new commit

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         rec r = new rec("nitesh",45);
-
+        rec t = new rec("RAjat",43);
+        System.out.println(r);
+        System.out.println(t);
+        System.out.println(r);
     }
 }
